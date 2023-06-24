b0659571dc923611a992e22dd144d30253240dbc
MarcoSteinke <marco.steinke@innoq.com>
2023-06-24 15:29:44 +0200

    setup project for oauth2

--- src/main/java/com/example/springoauth2demo/infrastructure/security/OAuth2LoginSecurity.java
+++ src/main/java/com/example/springoauth2demo/infrastructure/security/OAuth2LoginSecurity.java
@@ -1,2 +1,31 @@
-package com.example.springoauth2demo.infrastructure.security;public class OAuth2LoginSecurity {
+package com.example.springoauth2demo.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class OAuth2LoginSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll()
+                        //.requestMatchers("/").permitAll()
+                        //.anyRequest().authenticated()
+                ).oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
--- src/main/java/com/example/springoauth2demo/infrastructure/web/controller/OAuth2DemoController.java
+++ src/main/java/com/example/springoauth2demo/infrastructure/web/controller/OAuth2DemoController.java
@@ -1,2 +1,24 @@
-package com.example.springoauth2demo.infrastructure.web.controller;public class OAuth2DemoController {
+package com.example.springoauth2demo.infrastructure.web.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class OAuth2DemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/start")
+    @Secured("ROLE_USER")
+    public String start() {
+        return "start";
+    }
 }
