f68942a13e5d17eb339b963f66052289171cccea
MarcoSteinke <marco.steinke@innoq.com>
2023-06-24 15:55:48 +0200

    use @PreaAuthorize and import htmx via fragment

--- src/main/java/com/example/springoauth2demo/infrastructure/web/controller/OAuth2DemoController.java
+++ src/main/java/com/example/springoauth2demo/infrastructure/web/controller/OAuth2DemoController.java
@@ -1,12 +1,15 @@
 package com.example.springoauth2demo.infrastructure.web.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
 
 @Controller
 public class OAuth2DemoController {
@@ -17,8 +20,9 @@ public class OAuth2DemoController {
     }
 
     @GetMapping("/start")
-    @Secured("ROLE_USER")
-    public String start() {
+    @PreAuthorize("hasAuthority('OAUTH2_USER')")
+    public String start(Principal principal) {
+        System.out.println(principal);
         return "start";
     }
 }
